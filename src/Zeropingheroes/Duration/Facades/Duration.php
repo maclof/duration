0427c62a95ac2096291d1ac99d7fd0d7c3652388
ilumos <ilumos@gmail.com>
2014-06-08 21:54:52 +0100

    Initial commit

---
+++ src/Zeropingheroes/Duration/Facades/Duration.php
@@ -0,0 +1,14 @@
+<?php namespace Zeropingheroes\Duration\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Duration extends Facade {
+
+	/**
+	* Get the registered name of the component.
+	*
+	* @return string
+	*/
+	protected static function getFacadeAccessor() { return 'Duration'; }
+
+}
